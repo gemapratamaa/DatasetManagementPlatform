33d873a5b3f94545442d18f01301583a5da1d2e0
Gema Pratama Aditya <gemapratamaaditya@yahoo.com>
2021-08-17 12:47:35 +0700

    Update model to prevent bug when creating superuser

--- datasetmanagement/datasetmanager/models.py
+++ datasetmanagement/datasetmanager/models.py
@@ -5,40 +5,94 @@ from django.contrib.auth.models import AbstractUser, BaseUserManager, AbstractBa
 # Create your models here.
 # class DatasetUser(models.Model):
 class UserManager(BaseUserManager):
-    def create_user(self, username, password=None):
-
-        if username == '':
-            raise ValueError('Users must have a username')
+    def create_user(self, email, password=None):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
 
         user = self.model(
-            username=username,
+            email=self.normalize_email(email),
         )
 
         user.set_password(password)
         user.save(using=self._db)
         return user
 
+    def create_staffuser(self, email, password):
+        """
+        Creates and saves a staff user with the given email and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+        )
+        user.staff = True
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        """
+        Creates and saves a superuser with the given email and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+        )
+        user.staff = True
+        user.admin = True
+        user.save(using=self._db)
+        return user
+
+
 
 class User(AbstractBaseUser):
-
-    username = models.CharField(max_length=30, unique=True)
+    email = models.CharField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,)
     is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False) # a admin user; non super-user
-    is_admin = models.BooleanField(default=False) # a superuser
+    staff = models.BooleanField(default=False) # a admin user; non super-user
+    admin = models.BooleanField(default=False) # a superuser
     
     objects = UserManager()
 
-    USERNAME_FIELD = 'username'
-    
-    def __str__(self):
-        return self.username
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
-"""
-class CustomUser(AbstractBaseUser):
-    name = models.CharField(max_length=30)
+    def get_full_name(self):
+        # The user is identified by their email address
+        return self.email
+
+    def get_short_name(self):
+        # The user is identified by their email address
+        return self.email
+
     def __str__(self):
         return self.email
-"""
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        return self.staff
+
+    @property
+    def is_admin(self):
+        "Is the user a admin member?"
+        return self.admin
+
+
 
 class Task(models.Model):
     name = models.CharField(max_length=30)
@@ -61,5 +115,3 @@ class Dataset(models.Model):
         upload_to='datasets/', 
         validators=[FileExtensionValidator(allowed_extensions=['zip'])]
     )
-
-
