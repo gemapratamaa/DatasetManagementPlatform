03e2b3ed1e26da04df8e1cfee1056715f6901ab1
Gema Pratama Aditya <gemapratamaaditya@yahoo.com>
2021-08-18 13:33:40 +0700

    Add links to navigate pages in html

--- datasetmanagement/datasetmanager/forms.py
+++ datasetmanagement/datasetmanager/forms.py
@@ -1,21 +1,13 @@
 from django import forms
 from django.forms import ModelForm
 
-"""
-class DatasetUploadForm(forms.Form):
-    dataset_file = forms.FileField(
-        label='Select a file',
-    )
-"""
-
-
 class LoginForm(forms.Form):
     email = forms.EmailField(
-        label='email',
+        label='Email',
         required=True
     )
     password = forms.CharField(
-        label='password',
+        label='Password',
         required=True,
         widget=forms.PasswordInput()
     )
