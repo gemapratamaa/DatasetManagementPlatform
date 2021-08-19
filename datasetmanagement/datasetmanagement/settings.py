e338589ba0b4c29b209b420052a8ff496585ff2d
Gema Pratama Aditya <gemapratamaaditya@yahoo.com>
2021-08-19 11:49:16 +0700

    Add simple css file

--- datasetmanagement/datasetmanagement/settings.py
+++ datasetmanagement/datasetmanagement/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
 MEDIA_URL = '/datasets/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'datasets_cdn')
 
